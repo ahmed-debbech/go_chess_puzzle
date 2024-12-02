0bc4a7137a257268003bcd03da335ea81367c317
ahmed-debbech <debbech.ahmed@gmail.com>
2024-12-02 16:26:24 +0100

    refactor some stuff

--- backend/mongo/client.go
+++ backend/mongo/client.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"errors"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	_"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/ahmed-debbech/go_chess_puzzle/generator/data"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var uri string = readCreds();
@@ -29,20 +30,41 @@ func Init() {
 	client = oneShotClient()
 }
 
-func InsertPuzzle(puzzle data.Puzzle) string {
-	col := client.Database("official").Collection("puzzles")
+func MongoFindRandPuzzle() (data.Puzzle, error){
+	coll := client.Database("official").Collection("puzzles")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	res, err := col.InsertOne(ctx, puzzle)
-	
+	var result data.Puzzle
+	filter := mongo.Pipeline{{{Key: "$sample", Value: bson.D{{Key: "size", Value: 1}}}} }
+
+	cursor, err := coll.Aggregate(ctx, filter).Decode(&result)
 	if err != nil {
-		fmt.Println("[ERROR] puzzle ", puzzle.ID ," could not be saved in mongo because: ", err)
-		return ""
+		if err == mongo.ErrNoDocuments {
+			fmt.Println("[ERROR] the query succeeded but it seems like it is empty")
+			return result, errors.New("No Puzzles found")
+		}
+		fmt.Println("[ERROR] something went wrong while finding a random puzzle:", err)
+		return result, errors.New("Could not find random puzzle")
 	}
-	fmt.Println("[SUCCESS] saved puzzle ", puzzle.ID, " into mongo ", res.InsertedID)
-	return res.InsertedID.(primitive.ObjectID).String()
+	defer cursor.Close(ctx)
+
+	/*for cursor.Next(ctx) {
+		var result bson.M
+		if err := cursor.Decode(&result); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(result)
+	}*/
+
+	var results []bson.M
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Average price of %v \n", result["_id"])
+
+	return result, nil
 }
 
 func oneShotClient() *mongo.Client {
